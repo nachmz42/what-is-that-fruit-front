eeade69ebea21ab7be14012a1df49201d5cb2450
Nacho Martinez <nachomarzar@gmail.com>
2023-10-16 18:30:25 +0200

    Added page to upload your image

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,16 @@
-
-import Front from '@pages/Front';
-import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
+import Front from "@pages/Front";
+import UploadImage from "@pages/UploadImage/UploadImage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Front/>}></Route>
+        <Route path="/" element={<Front />}></Route>
+        <Route path="/upload-image" element={<UploadImage />}></Route>
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/UploadImage/UploadImage.tsx
@@ -0,0 +1,76 @@
+import { Upload, Button, message } from "antd";
+import { UploadOutlined } from "@ant-design/icons";
+import "./UploadImage.css";
+import { Divider, List, Typography } from "antd";
+
+const { Title, Paragraph } = Typography;
+
+const UploadImage = () => {
+  const props = {
+    name: "file",
+    action: "URL_de_tu_servidor_donde_manipular_las_imagenes",
+    headers: {
+      authorization: "authorization-text",
+    },
+    onChange(info: any) {
+      if (info.file.status !== "uploading") {
+        console.log(info.file, info.fileList);
+      }
+      if (info.file.status === "done") {
+        message.success(`${info.file.name} file uploaded successfully`);
+      } else if (info.file.status === "error") {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+  };
+
+  const fruits = [
+    "Apple",
+    "Banana",
+    "Cherry",
+    "Chickoo",
+    "Grapes",
+    "Kiwi",
+    "Mango",
+    "Orange",
+    "Strawberry",
+  ];
+
+  return (
+    <>
+      <Title
+        style={{ fontFamily: "Inter, sans-serif" }}
+        className="upload-button-container"
+      >
+        Here you can upload your photo
+      </Title>
+      <Paragraph
+        style={{ fontFamily: "Inter, sans-serif" }}
+        className="upload-button-container"
+      >
+        The image must be of one of these fruits:{" "}
+      </Paragraph>
+      <div className="list-container">
+        <div className="inner-container">
+          <Divider orientation="left">FRUITS</Divider>
+          <List
+            bordered
+            dataSource={fruits}
+            renderItem={(item) => (
+              <List.Item>
+                <Typography.Text mark></Typography.Text> {item}
+              </List.Item>
+            )}
+          />
+        </div>
+      </div>
+      <div className="upload-button-container">
+        <Upload {...props}>
+          <Button icon={<UploadOutlined />}>Upload Image</Button>
+        </Upload>
+      </div>
+    </>
+  );
+};
+
+export default UploadImage;
